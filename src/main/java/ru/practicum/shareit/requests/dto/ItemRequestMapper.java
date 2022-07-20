b756cda049f749a3b62a1a24b98194e59eabe9f3
Sadaaaaa <sadopwnz@yandex.ru>
2022-07-20 10:11:20 +0300

    fix: del not found maven plugins

--- src/main/java/ru/practicum/shareit/requests/dto/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/requests/dto/ItemRequestMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.requests.dto;
 
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.requests.ItemRequest;
 
 public class ItemRequestMapper {
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
-
 import javax.validation.Valid;
 import java.util.List;
 
