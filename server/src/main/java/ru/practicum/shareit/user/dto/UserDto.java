d07f99dc18999fb4fafc2ebb61c679f7140d9d2e
Sadaaaaa <sadopwnz@yandex.ru>
2022-09-04 14:10:34 +0300

    fix: userController

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,23 +1,22 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
-@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
     private int id;
     private String name;
     @NotEmpty(message = "Email shouldn't be empty")
     @Email(message = "Email should be valid")
     private String email;
-
-    public UserDto(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 }
 
 
